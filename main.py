17a01628d4b5e3d4f6e7107db1b92a0e7f27aba7
nortoh <christian@christianhorton.me>
2021-10-18 08:26:22 -0400

    Preparing to use multiprocessing. Also we like queues

--- main.py
+++ main.py
@@ -3,29 +3,41 @@ import logging
 import sys, getopt
 import cv2
 import time
+import queue
 import numpy as np
+import multiprocessing as mp
+from multiprocessing import Process
+
 from collections import deque
 
 class Main(object):
 
     def __init__(self):
         print("template_matcher (nortoh) v1.0")
+
+        # Data structures
+        self.image_frames = deque()
+        self.found_frames = deque()
+
+        # Settings
         self.video = 'movie.mp4'
         self.template_image_file = 'template2.png'
         self.template = cv2.imread(self.template_image_file)
-        self.image_frames = deque()
-        self.found_frames = deque()
-        
 
-    def execute(self):
+    def start_processes(self):
+        self.load_video_process = Process(target=self.load_video)
+        self.template_match_process = Process(target=self.perform_vide_check)
+        self.load_video_process.start()
+        self.template_match_process.start()
+
+    def load_video(self):
         cam = cv2.VideoCapture(self.video)
 
         start_time = time.perf_counter()
 
-        # FPS and frames
+        # FPS and frames ggh 
         self.fps = cam.get(cv2.CAP_PROP_FPS)
         self.frame_count = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
-
         # Width and height
 
         self.frame_width = int(cam.get(3))
@@ -39,17 +51,18 @@ class Main(object):
         
         # Stick to a small section of frames for debugging
         current_frame = 0
-        
+        max_historic_count = 1000
+
         while(current_frame < self.frame_count):
             ret, image = cam.read()
 
+            # Handle frame
             if ret:
                 # We have a good frame
                 check_time = time.perf_counter()
-                # Check frame
-                # print(f'Checking frame: {current_frame}')
-                self.check_frame(current_frame, image)
-                
+
+                self.image_frames.append((current_frame, image))
+
                 if (current_frame % 100 == 1):
                     # duration
                     finished_time = current_frame / self.fps
@@ -61,21 +74,31 @@ class Main(object):
                     run_time = end_time - start_time
                     match_time = end_time - check_time
                     print(f'[({current_frame}/{self.frame_count}) Runtime: {run_time:0.4f}] ({str(round(finished_minutes, 2))}:{str(round(finished_seconds, 2))}/{str(round(duration_minutes, 2))}:{str(round(duration_seconds, 2))})')
-
+                
                 current_frame += 1
             else:
                 # We have a bad frame
                 break
+            
+            # We need to start pushing out frames we don't need
+            if len(self.image_frames) > max_historic_count:
+                self.image_frames.pop()
 
-        # Clean upa        
+        # Clean up
         cam.release()
         cv2.destroyAllWindows()
-
+        
         self.clip_and_save()
 
         end_time = time.perf_counter()
         run_time = end_time - start_time
         print(f"Total runtime: {run_time:0.4f} seconds")
+
+    def perform_check(self):
+        while len(self.image_frames) != 0:
+            current_frame, image = self.image_frames.get()
+
+            self.check_frame(current_frame, image)
 
 
     def check_frame(self, current_frame, image):
@@ -114,4 +137,4 @@ class Main(object):
         output_video.release()
 
 if __name__ == '__main__':
-    Main().execute()
+    Main().start_processes()
